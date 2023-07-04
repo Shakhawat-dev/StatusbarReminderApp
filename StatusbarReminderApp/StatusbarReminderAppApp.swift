c59bc5c6a16fc3405002efb1c57be8cd0471d6b2
Md Shakhawat Hossain Shahin <shakhawat.dev@gmail.com>
2023-07-04 09:51:22 +0600

    Initial Commit

---
+++ StatusbarReminderApp/StatusbarReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusbarReminderAppApp.swift
+//  StatusbarReminderApp
+//
+//  Created by Shakhawat Hossain Shahin on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StatusbarReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
